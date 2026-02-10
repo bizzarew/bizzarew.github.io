40e398dfe32eb3da7149422aa10bba9db8f9e727
Maya <120439889+bizzarew@users.noreply.github.com>
2026-02-10 15:52:19 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -27,13 +27,25 @@ var init = function (window) {
       circles.push(circle);
     }
     // TODO 3 : Call the drawCircle() function
-    drawCircle(0);
-    drawCircle(1);
-    drawCircle(2);
-    drawCircle(3);
-    drawCircle(4);
+    //drawCircle(0);
+    //drawCircle(1);
+    //drawCircle(2);
+    //drawCircle(3);
+    //drawCircle(4);
 
     // TODO 7 : Use a loop to create multiple circles
+    for (var i = 0; i < 50; i++) {
+      drawCircle();
+      // Initialize gamification features (unlocked after completing educational TODOs)
+      Gamification.init({
+        canvas: canvas,
+        view: view,
+        draw: draw,
+        physikz: physikz,
+        circles: circles,
+        game: game,
+      });
+    }
 
     ///////////////////
     // PROGRAM LOGIC //
@@ -44,22 +56,28 @@ var init = function (window) {
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
-    function update() {
-      physikz.updatePosition(circles[0]);
-      physikz.updatePosition(circles[1]);
-      physikz.updatePosition(circles[2]);
-      physikz.updatePosition(circles[3]);
-      physikz.updatePosition(circles[4]);
 
-      // TODO 4 : Update the position of each circle using physikz.updatePosition()
+    // TODO 4 : Update the position of each circle using physikz.updatePosition()
+    function update() {
+      //physikz.updatePosition(circles[0]);
+      //physikz.updatePosition(circles[1]);
+      //physikz.updatePosition(circles[2]);
+      //physikz.updatePosition(circles[3]);
+      //physikz.updatePosition(circles[4]);
 
       // TODO 5 : Call game.checkCirclePosition() on your circles
-      game.checkCirclePosition(circles[0]);
-      game.checkCirclePosition(circles[1]);
-      game.checkCirclePosition(circles[2]);
-      game.checkCirclePosition(circles[3]);
-      game.checkCirclePosition(circles[4]);
+      //game.checkCirclePosition(circles[0]);
+      //game.checkCirclePosition(circles[1]);
+      //game.checkCirclePosition(circles[2]);
+      //game.checkCirclePosition(circles[3]);
+      //game.checkCirclePosition(circles[4]);
       // TODO 8 / TODO 9 : Iterate over the array
+      for (var i = 0; i < circles.length; i++) {
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+        // Update gamification features each frame
+        Gamification.update();
+      }
     }
 
     /* 
@@ -72,18 +90,17 @@ var init = function (window) {
       if (circle.x > canvas.width) {
         circle.x = 0;
       }
-      
-      
-
-      // TODO 6 : YOUR CODE STARTS HERE //////////////////////
       if (circle.y > canvas.height) {
         circle.y = 0;
       }
-      
-      
 
-
-
+      // TODO 6 : YOUR CODE STARTS HERE //////////////////////
+      if (circle.x < 0) {
+        circle.x = canvas.width;
+      }
+      if (circle.y < 0) {
+        circle.y = canvas.height;
+      }
 
       // YOUR TODO 6 CODE ENDS HERE //////////////////////////
     };
